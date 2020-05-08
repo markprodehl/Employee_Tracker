6f968d86be347d8a612e88ad64516f89409aa9d0
Mark Prodehl <markprodehl@gmail.com>
2020-05-07 18:33:09 -0700

    deleted unwanted files and printed hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import About from "./components/pages/About";
-import Discover from "./components/pages/Discover";
-import Search from "./components/pages/Search";
-
+import React from 'react';
+// import './App.css';
+import Main from "./components/Main";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Route exact path="/" component={About} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/discover" component={Discover} />
-        <Route path="/search" component={Search} />
-      </div>
-    </Router>
-  );
+  return <Main />;
 }
 
 export default App;
