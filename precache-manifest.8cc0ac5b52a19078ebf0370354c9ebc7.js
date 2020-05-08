feccd8f952c320cf6e80e4472422336846bc0204
Mark Prodehl <markprodehl@gmail.com>
2020-05-08 16:36:44 -0700

    Updates

---
+++ precache-manifest.8cc0ac5b52a19078ebf0370354c9ebc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596b1d3b41b813467f050aa1bfa3ae52",
+    "url": "/Employee_Tracker/index.html"
+  },
+  {
+    "revision": "1e2a66fe4845312b8514",
+    "url": "/Employee_Tracker/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "1e2a66fe4845312b8514",
+    "url": "/Employee_Tracker/static/js/2.f9e6341c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee_Tracker/static/js/2.f9e6341c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b626197ae4b749954fc",
+    "url": "/Employee_Tracker/static/js/main.cae865b5.chunk.js"
+  },
+  {
+    "revision": "bda9d9e267b2e4b0f824",
+    "url": "/Employee_Tracker/static/js/runtime-main.af5a94d0.js"
+  }
+]);
